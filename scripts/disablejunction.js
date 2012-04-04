70d3ef49a9a94b2bd6b6964284ea389ebc1771ce
liu yongfeng <michalliu@foxmail.com>
2012-04-04 17:14:02 +0800

    first version

---
+++ scripts/disablejunction.js
@@ -0,0 +1,28 @@
+document.body.addEventListener("DOMSubtreeModified",function (e) {
+
+    var i=0,
+
+        l,
+
+    	md,
+
+    	mda = "onmousedown",
+
+        links = Sizzle(".g .r a");
+
+    if (links.length >= 0) {
+
+    	for(;i < links.length;i++) {
+
+    		l = links[i],
+
+    		md = l.getAttribute(mda);
+
+    		if (md) { l.removeAttribute(mda) }
+
+    	}
+
+    }
+
+});
+
