bed0886278ca01d83872e625a2f19bcb98199e43
liu yongfeng <michalliu@foxmail.com>
2012-11-05 23:21:28 +0800

    增加提示功能

---
+++ scripts/background.js
@@ -0,0 +1,59 @@
+/*global chrome,document,console,localStorage*/
+// This function creates a new anchor element and uses location
+// properties (inherent) to get the desired URL data. Some String
+// operations are used (to normalize results across browsers).
+ 
+function parseURL(url) {
+    var a =  document.createElement('a');
+    a.href = url;
+    return {
+        source: url,
+        protocol: a.protocol.replace(':',''),
+        host: a.hostname,
+        port: a.port,
+        query: a.search,
+        params: (function(){
+            var ret = {},
+                seg = a.search.replace(/^\?/,'').split('&'),
+                len = seg.length, i = 0, s;
+            for (;i<len;i++) {
+                if (!seg[i]) { continue; }
+                s = seg[i].split('=');
+                ret[s[0]] = s[1];
+            }
+            return ret;
+        })()
+     };
+}
+
+function generateSafeGoogleSearchURL(q) {
+    return 'https://www.google.com.hk/search?q=' + q;
+}
+
+chrome.webRequest.onErrorOccurred.addListener(
+  function(details) {
+      var handledErrors = ['net::ERR_CONNECTION_RESET', 'net::ERR_EMPTY_RESPONSE'];
+      var q, tabid;
+      var key = 'automaticRedirectTipsShown';
+      var automaticRedirectTipsShown = localStorage.getItem(key);
+      if (handledErrors.indexOf(details.error) !== -1) {
+          q = decodeURIComponent(parseURL(details.url).params.q);
+          tabid = details.tabId;
+          chrome.tabs.update(tabid,{url: generateSafeGoogleSearchURL(q)});
+          if (!automaticRedirectTipsShown) {
+              chrome.tabs.executeScript(tabid, { file: '/scripts/showtip.js'});
+              localStorage.setItem(key,1);
+          }
+      }
+  },
+  {urls: ['<all_urls>'],types:['main_frame']});
+
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
+    var data;
+    if (request.method === 'getLocalStorage') {
+        data = localStorage.getItem(request.key);
+    } else if(request.method === 'setLocalStorage') {
+        localStorage.setItem(request.key,request.data);
+    }
+    sendResponse({data: data});
+});
--- scripts/disablejunction.js
+++ scripts/disablejunction.js
@@ -1,4 +1,5 @@
-document.body.addEventListener("DOMSubtreeModified",function (e) {
+/*global document,console,chrome,Sizzle*/
+document.body.addEventListener('DOMSubtreeModified',function () {
 
     var i=0,
 
@@ -6,9 +7,9 @@ document.body.addEventListener("DOMSubtreeModified",function (e) {
 
         md,
 
-        mda = "onmousedown",
+        mda = 'onmousedown',
 
-        links = Sizzle(".g .r a");
+        links = Sizzle('.g .r a');
 
     if (links.length >= 0) {
 
@@ -24,5 +25,24 @@ document.body.addEventListener("DOMSubtreeModified",function (e) {
 
     }
 
+});
+
+function showTips() {
+    var appbar = document.getElementById('appbar');
+    var tips = document.createElement('div');
+    tips.style.cssText='margin:17px auto 0px;width:500px;color:#468847;background-color:#DFF0D8;margin-top:17px;padding:8px 35px 8px 14px;text-shadow:0 1px 0 rgba(255, 255, 255, 0.5);border:1px solid #FBEED5;border-color:#D6E9C6;font-size:14px;line-height:20px;border-radius:4px;';
+    tips.innerHTML = '谷歌搜索助手使您可以直连搜索结果中的网址，避免使用谷歌服务器的跳转服务，以加快网页的打开速度和避免发生意外。此提示只会出现一次。<a href="#" onclick="this.parentNode.parentNode.removeChild(this.parentNode);return false;">我知道了</a>';
+    appbar.parentNode.insertBefore(tips,appbar);
+}
+
+var key = 'disableJunctionTipsShown';
+
+chrome.extension.sendRequest({method: 'getLocalStorage', key: key}, function (response) {
+	console.log(response);
+    var value = response.data;
+    if (!value) {
+        showTips();
+        chrome.extension.sendRequest({method: 'setLocalStorage', key: key, data: 1});
+    }
 });
 
